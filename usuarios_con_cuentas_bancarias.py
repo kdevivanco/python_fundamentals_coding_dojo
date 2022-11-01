f44cf0a0243e9a20071062bb18fb6c2dfccd3a86
MacBookPro <macbookpro@MacBook-Pro-de-MacBookPro.local>
2022-10-31 19:36:32 -0500

    Funcionalidad completa

--- usuarios_con_cuentas_bancarias.py
+++ usuarios_con_cuentas_bancarias.py
@@ -14,20 +14,21 @@ class CuentaBancaria:
         
     def deposito(self, amount):
         self.balance += amount
-        print(f'Depositaste {amount} a tu cuenta')
+        print(f'Depositaste {amount}')
         return self
     
     def retiro(self, amount):
         if amount>self.balance:
             self.balance -= 5
             print('Fondos insuficientes: cobrando una tarifa de $5')
+            return 1
         else:
             self.balance -= amount
-            print(f'Retiraste {amount} a tu cuenta')
+            print(f'Retiraste {amount}')
         return self
     
     def mostrar_balance(self):
-        print(f'Balance: ${self.balance}, esta en {self.name}')
+        print(f'Balance: ${self.balance}')
         return self
     
     def generar_interés(self):
@@ -51,22 +52,30 @@ class Usuario:
     instancias = []
     def __init__(self, titular):
         self.titular = titular
-        Usuario.instancias.append(self.titular)
+        Usuario.instancias.append(self)
         self.cuentas = []
         self.cuentastuple = []
 
-    def crear_cuenta(self,tipo):
-        #tipo = input('Escoge tipo de cuenta(ahorros, corriente): ')
+    def crear_cuenta(self):
+        tipo = input('Escoge tipo de cuenta(ahorros, corriente): ')
         self.cuentas.append(CuentaBancaria(tipo))
         print(f'Cuenta en {tipo} creada!')
         nrodecuenta = ''
     
-    def verificacion_cuenta(self,numero_de_cuenta): #Se agrego metodo verificacion_cuenta: nos retorna la cuenta correspondiente al nro
+    def verificacion_cuenta(self,numero_de_cuenta):
         for i in range(len(self.cuentas)):
             if  str(self.cuentas[i].nrodecuenta) == str(numero_de_cuenta):
                 return self.cuentas[i]
             else:
                 print('Cuenta no encontrada')
+                return False
+                
+    def verificacion_destinatario(self,numero_de_cuenta):
+        for usuario in Usuario.instancias: #chequea todos los nro de cuenta
+            for cuenta in usuario.cuentas: #por cuenta en instancia
+                if str(cuenta.nrodecuenta) == str(numero_de_cuenta):
+                    print('Esta cuenta existe!')
+                    return cuenta, usuario #devuelve toda la cuenta objeto CuentaBancaria
             
     def hacer_deposito(self,amount,numero_de_cuenta):
         cuenta = self.verificacion_cuenta(numero_de_cuenta)
@@ -74,41 +83,63 @@ class Usuario:
 
     def hacer_retiro(self,amount,numero_de_cuenta):
         cuenta = self.verificacion_cuenta(numero_de_cuenta)
-        cuenta.retiro(amount)
+        respuesta = cuenta.retiro(amount)
+        return respuesta #nos devuelve un valor 1 si la transaccion fue denegada
         
     def mostrar_balance(self,numero_de_cuenta):
         cuenta = self.verificacion_cuenta(numero_de_cuenta)
         cuenta.mostrar_balance()
 
-    def mostrar_cuentas(self): #Se modifico el metodo mostrar cuentas para mostrar la info completa
+    def mostrar_cuentas(self):
         i=0
         for i in range(len(self.cuentas)):
-            cuenta = self.cuentas[i] #asignacion de variable para codigo mas limpio
+            cuenta = self.cuentas[i]
             print(f'{self.titular}, {cuenta.name} {cuenta.nrodecuenta} | $ {cuenta.balance}')
             
 
-    def transfer_dinero(self, nrousuario, nrodestinatario, amount): 
+    def transfer_dinero(self): 
+        #Ingresa el usuario
         nrousuario = input("Ingresa tu numero de cuenta: ")
+        
+        #Verifica el usuario
+        solicitud_numero = self.verificacion_cuenta(nrousuario)
+        if solicitud_numero == False:
+            return
+        
+        #Ingresa el destinatario
         nrodestinatario = input("Ingresa numero de cuenta del destinatario: ")
         
-        if nrodestinatario not in CuentaBancaria.todos_los_numeros:
-            print('Esta cuenta no existe')
+        #verifica el destinatario
+        NoneType = type(None)
+        res_verificacion = self.verificacion_destinatario(nrodestinatario)
+        
+        if NoneType == type(res_verificacion): 
+            print('Cuenta no encontrada')
+            return
+        #Empieza la transaccion
         else: 
-            for cuenta in CuentaBancaria.todas_las_cuentas:
-                verificacion = (f'El destinatario es: {cuenta.name}? Ingresa (Si o No)')
-                respuesta = input(verificacion)
+            cuenta,usuario = res_verificacion
+            print(cuenta,usuario)
+            #Verifica el nombre de destinatario
+            verificacion = (f'El destinatario es: {usuario.titular}? Ingresa (Si o No)')
+            respuesta = input(verificacion)
+            if respuesta.lower() == 'si':
+                monto_str = input('Ingrese monto')
+                monto_a_depositar = int(monto_str)
+                #Verifica el monto / retira de cuenta usuario
+                solicitud_monto = self.hacer_retiro(monto_a_depositar,nrousuario)
+                if solicitud_monto == 1: # denegada
+                    print ('DENEGADO')
+                    return
+                else: #deposita (transfiere) al destinatario
+                    usuario.hacer_deposito(monto_a_depositar,nrodestinatario)
+            else: 
+                print('Vuelva a intentarlo')
+                return
                 
-                if respuesta.lower() == 'si':
-                    monto_a_depositar = input('Ingrese monto')
-                    self.hacer_retiro(monto_a_depositar,nrousuario)
-                    
-                    
-                else:
-                    print('Vuelva a intentarlo')
-                    break
-#Proximos pasos: 
-#1. Agregar funcion "verificacion" donde se verifica que el numero de cuenta ingresado es correcto
-#2. Agregar esta funcion a cada metodo de CuentaBancaria o Usuario (por verse) para no tener que repetir codigo
-#3. Terminar de construir el metodo transferir dinero 
+
+
+kayla = Usuario('Kayla de Vivanco')
+kayla.crear_cuenta()
 
 
